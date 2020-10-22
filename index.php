0a1f3590688a3305031646e7d9a027158cdfe30b
Lennart Elbe <lenni.elbe@gmail.com>
2020-10-22 18:01:57 +0200

    index'

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<HTML>
+<HEAD>
+<TITLE>PRACTICE</TITLE>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+    <script src="libraries/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="libraries/jquery-3.5.1.min.js"></script>
+    <script src="libraries/qrCodeLib/qrcode.js" type="text/javascript"></script>
+    <script type="text/javascript" src="./js/qrPlayground.js"></script>
+</HEAD>
+
+<BODY>
+    
+    <input id="text" type="text" value="https://hogangnono.com" style="width:80%" /><br />
+    <div id="qrcode"></div>
+    <div id="here_table">
+        <table style="width:100%">
+            <tr>
+                <th>Firstname</th>
+                <th>Lastname</th>
+                <th>Age</th>
+            </tr>
+            <tr>
+                <td>Jill</td>
+                <td>Smith</td>
+                <td>50</td>
+            </tr>
+            <tr>
+                <td>Eve</td>
+                <td>Jackson</td>
+                <td>94</td>
+            </tr>
+        </table>
+    </div>
+    
+</BODY>
+
+</HTML>
